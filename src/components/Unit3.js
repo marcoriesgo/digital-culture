222fc1757855d2a5f07588edd3f86586499f81e6
Marco Riesgo Almonacid <marcoriesgo@Marcos-MBP-2.lan>
2020-10-12 18:21:05 -0700

    done

--- src/components/Unit3.js
+++ src/components/Unit3.js
@@ -95,13 +95,12 @@ class Unit3 extends Component {
                 <a href="https://coggle.it/diagram/X1bGwr2x3ka7jkx7/t/capstone-project-map-critical-consumption/4cc0fcf3532e715c6cfc5e21090eb11f9324505bf7c111e7d61fce8f8e3544c8" target="_blank" className="resource-link">View Marco's Mind Map on Coggle</a>
                 
                 <h2 className="resources-section-header">Apps for Learning to Code </h2>
-                <ul>
-                    <a href = "https://www.udacity.com/" target="_blank" className="app-link">Udacity</a> <br />
-                    <a href = "https://www.udemy.com/" target="_blank" className="app-link">Udemy</a><br />
-                    <a href = "https://codehubplatform.github.io/" target="_blank" className="app-link">Codehub</a><br />
-                    <a href = "https://www.programming-hero.com/" target="_blank" className="app-link">Programming Hero</a><br />
-                    <a href = "https://www.sololearn.com/" target="_blank" className="app-link">Sololearn</a>
-                </ul>
+                <a href = "https://www.udacity.com/" target="_blank" className="app-link">Udacity</a> <br />
+                <a href = "https://www.udemy.com/" target="_blank" className="app-link">Udemy</a><br />
+                <a href = "https://codehubplatform.github.io/" target="_blank" className="app-link">Codehub</a><br />
+                <a href = "https://www.programming-hero.com/" target="_blank" className="app-link">Programming Hero</a><br />
+                <a href = "https://www.sololearn.com/" target="_blank" className="app-link">Sololearn</a>
+                
                 
                 <h2 className="resources-section-header">Contact</h2>
                 <p className="resources-section-content">Interested in learning even more about the current state of coding education? I am always happy to discuss new ideas about the future of coding education. Feel free to reach out to me at the link below!</p>
