c02ee69a58d9cbf8d66e40d230be27ebb13d4d2e
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-10-27 18:13:53 -0700

    h

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,7 +17,7 @@ class Home extends Component {
             <h3>By Marco Riesgo</h3>
           </div>
 
-          <div className="content-box-container">
+          <div className="content-box-container-home">
                     <h2 className="project-content-header">Letter From the Editor</h2>
                     <p className="personal-reflection-content">
                       Hello everyone and welcome to this podcast. Today, we are going to be discussing the concept of positive obsessions. Then, I will dive deeper into my own personal positive obsession and passion for education equity in the United States. As Octavia Butler describes it, a positive obsession is an uncontrollable passion one feels towards a mission. Unlike a normal hobby one might have, we feel as though we are unable to control it. Oftentimes, our positive obsession is a major part of our identity. In a way, our positive obsession guides the way for us to build our identity as we build connections and learn more about the topics we are passionate about.
