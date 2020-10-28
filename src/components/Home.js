133cd562808bbabb2a550e9b61d301162e6503f8
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-10-27 18:26:51 -0700

    working on script

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 
 import poster from "../img/poster.png";
 
+import narrativePodcast from "../img/narrativePodcast.mp3";
+
+
 class Home extends Component {
   render() {
     return (
@@ -18,12 +21,18 @@ class Home extends Component {
           </div>
 
           <div className="content-box-container-home">
-                    <h2 className="project-content-header">Letter From the Editor</h2>
-                    <p className="personal-reflection-content">
-                      Hello everyone and welcome to this podcast. Today, we are going to be discussing the concept of positive obsessions. Then, I will dive deeper into my own personal positive obsession and passion for education equity in the United States. As Octavia Butler describes it, a positive obsession is an uncontrollable passion one feels towards a mission. Unlike a normal hobby one might have, we feel as though we are unable to control it. Oftentimes, our positive obsession is a major part of our identity. In a way, our positive obsession guides the way for us to build our identity as we build connections and learn more about the topics we are passionate about.
-                    </p>
-                    
-            </div>
+            <h2 className="project-content-header">Letter From the Editor</h2>
+            <p className="personal-reflection-content">
+              Hello everyone and welcome to this podcast. Today, we are going to be discussing the concept of positive obsessions. Then, I will dive deeper into my own personal positive obsession and passion for education equity in the United States. As Octavia Butler describes it, a positive obsession is an uncontrollable passion one feels towards a mission. Unlike a normal hobby one might have, we feel as though we are unable to control it. Oftentimes, our positive obsession is a major part of our identity. In a way, our positive obsession guides the way for us to build our identity as we build connections and learn more about the topics we are passionate about.
+            </p>            
+          </div>
+
+          <div className="content-box-container-home">
+            <h2 className="project-content-header">Digital Literacy (Meta)Narrative</h2>
+            <audio controls>
+              <source src={narrativePodcast} type="audio/mpeg"/>
+            </audio>  
+          </div>
 
         </div>
       </div>
