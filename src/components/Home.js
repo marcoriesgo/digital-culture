4e9e0f27a56e516dd8d388962b594b8ce4c4dc58
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-10-27 17:40:17 -0700

    completing home

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,15 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-        <h1>Digital Literacy and Cultural Change</h1>
-        <h3>By Marco Riesgo</h3>
-        <div className="home-image-container">
-            <img src={poster} alt="Digital Culture" className="project-screenshot"/>
-        </div>
-        <div className="check-back-message">
-            <p>Hello and welcome to Digital Literacy and Cultural Change. Through a series of four unit projects, we will be exploring the tools and content that we use to consume information online. We will be using several different communication tools to help us learn more about how technology is changing the way we communicate by looking at things such as genre, rhetoric, and purpose. Click on the  projects above to explore digital communication and how it affects us as a society.</p>
+        <div className="home-content-container">
+          <h1>Digital Literacy and Cultural Change</h1>
+          <h3>By Marco Riesgo</h3>
+          <div className="home-image-container">
+              <img src={poster} alt="Digital Culture" className="project-screenshot"/>
+          </div>
+          <div className="check-back-message">
+              <p>Hello and welcome to Digital Literacy and Cultural Change. Through a series of four unit projects, we will be exploring the tools and content that we use to consume information online. We will be using several different communication tools to help us learn more about how technology is changing the way we communicate by looking at things such as genre, rhetoric, and purpose. Click on the  projects above to explore digital communication and how it affects us as a society.</p>
+          </div>
         </div>
       </div>
     );
