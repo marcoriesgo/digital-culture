06e0045b6f48e201e5eae8ae71d77e2059225d12
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-10-27 17:56:27 -0700

    working hom

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,15 +7,12 @@ class Home extends Component {
     return (
       <div className="home">
         <div className="home-content-container">
-          <h1 className="home">Digital Literacy and Cultural Change</h1>
+          <h1>Digital Literacy and Cultural Change</h1>
           <h3>By Marco Riesgo</h3>
           <div className="home-image-container">
               <img src={poster} alt="Digital Culture" className="project-screenshot"/>
           </div>
-          <div className="content-box-container">
-            <h2 className="project-content-header">Letter from the Editor</h2>
-                   
-          </div>
+          
         </div>
       </div>
     );
